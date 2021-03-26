68099be40d9b345b4f857734581a6c1cecbace26
Tiago Faxina <thfxina@gmail.com>
2021-03-26 20:17:26 -0300

    Styled posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FiUser, FiCalendar } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +27,92 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Posts</title>
+      </Head>
+      <div className={styles.container}>
+        <img src="/Logo.svg" alt="logo" />
+        <main className={styles.posts}>
+          <Link key="'" href="/">
+            <a>
+              <div className={styles.post}>
+                <h1>Como utilizar Hooks</h1>
+                <p>Pensando em sincronização em vez de ciclos de vida.</p>
+                <div className={styles.info}>
+                  <div className={styles.dateInfo}>
+                    <FiCalendar color="#bbbbbb" />
+                    <time>15 Mar 2021</time>
+                  </div>
+                  <div className={styles.userInfo}>
+                    <FiUser color="#bbbbbb" />
+                    <span>Joseph Oliveira</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link key="'" href="/">
+            <a>
+              <div className={styles.post}>
+                <h1>Como utilizar Hooks</h1>
+                <p>Pensando em sincronização em vez de ciclos de vida.</p>
+                <div className={styles.info}>
+                  <div className={styles.dateInfo}>
+                    <FiCalendar color="#bbbbbb" />
+                    <time>15 Mar 2021</time>
+                  </div>
+                  <div className={styles.userInfo}>
+                    <FiUser color="#bbbbbb" />
+                    <span>Joseph Oliveira</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link key="'" href="/">
+            <a>
+              <div className={styles.post}>
+                <h1>Como utilizar Hooks</h1>
+                <p>Pensando em sincronização em vez de ciclos de vida.</p>
+                <div className={styles.info}>
+                  <div className={styles.dateInfo}>
+                    <FiCalendar color="#bbbbbb" />
+                    <time>15 Mar 2021</time>
+                  </div>
+                  <div className={styles.userInfo}>
+                    <FiUser color="#bbbbbb" />
+                    <span>Joseph Oliveira</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link key="'" href="/">
+            <a>
+              <div className={styles.post}>
+                <h1>Como utilizar Hooks</h1>
+                <p>Pensando em sincronização em vez de ciclos de vida.</p>
+                <div className={styles.info}>
+                  <div className={styles.dateInfo}>
+                    <FiCalendar color="#bbbbbb" />
+                    <time>15 Mar 2021</time>
+                  </div>
+                  <div className={styles.userInfo}>
+                    <FiUser color="#bbbbbb" />
+                    <span>Joseph Oliveira</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </main>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
