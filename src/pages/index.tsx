7bd0779c627c9046c2a2bab767312e0d8d5f4f2a
Tiago Faxina <thfxina@gmail.com>
2021-03-27 00:42:42 -0300

    Adjsuted posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import { FiUser, FiCalendar } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
 
+import { useEffect, useState } from 'react';
+import ApiSearchResponse from '@prismicio/client/types/ApiSearchResponse';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -27,9 +30,67 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
+const formatPostsPagination = (postsResponse: ApiSearchResponse) => {
+  const { next_page, results } = postsResponse;
+
+  const newResults = results.map(post => ({
+    uid: post.uid,
+    first_publication_date: new Date(
+      post.first_publication_date
+    ).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
+    data: post.data,
+  }));
+
+  return {
+    results: newResults,
+    next_page,
+  };
+};
+
+const formatPostsPaginationFront = (
+  postsResponse: ApiSearchResponse,
+  nextPage?: string
+) => {
+  const { next_page, results } = formatPostsPagination(postsResponse);
+
+  return {
+    next_page: next_page
+      ? `${next_page}&access_token=${nextPage?.split('access_token=')[1]}`
+      : null,
+    results,
+  };
+};
+
 export default function Home({
-  postsPagination: { results } = {} as PostPagination,
+  postsPagination: { results, next_page } = {} as PostPagination,
 }: HomeProps) {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [nextPage, setNextPage] = useState<string>();
+
+  useEffect(() => {
+    setPosts(results);
+    setNextPage(next_page);
+  }, [results, next_page]);
+
+  const handleShowMorePostButton = async () => {
+    try {
+      const response = await fetch(nextPage);
+      const data = await response.json();
+      const {
+        results: newPosts,
+        next_page: newNextPage,
+      } = formatPostsPaginationFront(data, nextPage);
+      setPosts([...posts, ...newPosts]);
+      setNextPage(newNextPage);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -38,7 +99,7 @@ export default function Home({
       <div className={styles.container}>
         <img src="/Logo.svg" alt="logo" />
         <main className={styles.posts}>
-          {results?.map(post => (
+          {posts?.map(post => (
             <Link key={post.uid} href="/">
               <a>
                 <div className={styles.post}>
@@ -59,14 +120,43 @@ export default function Home({
             </Link>
           ))}
         </main>
+        {nextPage && (
+          <button
+            type="button"
+            className={styles.showMorePostsButton}
+            onClick={handleShowMorePostButton}
+          >
+            Carregar mais posts
+          </button>
+        )}
       </div>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+const formatPostsPaginationServer = (postsResponse: ApiSearchResponse) => {
+  const { next_page, results } = formatPostsPagination(postsResponse);
 
-//   // TODO
-// };
+  return {
+    next_page: `${next_page}&access_token=${process.env.PRISMIC_ACCESS_TOKEN}`,
+    results,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.predicates.at('document.type', 'posts'),
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 1,
+    }
+  );
+  // console.log(JSON.stringify(postsResponse, null, 2));
+
+  return {
+    props: {
+      postsPagination: formatPostsPaginationServer(postsResponse),
+    },
+  };
+};
